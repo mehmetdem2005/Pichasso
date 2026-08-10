965b8fc85727e6a4f60233aa1550ddbb3976b775
mehmetdem2005 <mehmetdem2005@gmail.com>
2026-08-10 20:25:05 +0300

    Remove legacy cards service

--- apps/api/src/modules/cards/cards.service.mjs
+++
@@ -1,8 +0,0 @@
-export class CardsService {
-  constructor(repository) { this.repository = repository; }
-
-  async getPublishedCards(context = {}) {
-    const cards = await this.repository.listPublished(context);
-    return { cards };
-  }
-}
